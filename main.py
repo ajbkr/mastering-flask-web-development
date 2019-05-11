ac5904e05a9b9ea7d92f7615a0aa6ab5cd5c8f39
Andrew Baker <andrew_j_baker2@hotmail.com>
2019-05-11 10:03:07 +0100

    Add Comment and Post models

--- main.py
+++ main.py
@@ -13,10 +13,49 @@ def home():
 if __name__ == '__main__':
     app.run()
 
+@db.event.listens_for(db.engine, 'connect')
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute('PRAGMA foreign_keys=ON')
+    cursor.close()
+
+class Comment(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    date = db.Column(db.DateTime())
+    post_id = db.Column(db.Integer(), db.ForeignKey('post.id'))
+
+    def __repr__(self):
+        return "<Comment '{}'>".format(self.text[:15])
+
+class Post(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    publish_date = db.Column(db.DateTime())
+    comments = db.relationship(
+        'Comment',
+        backref='post',
+        lazy='dynamic'
+    )
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Post '{}'>".format(self.title)
+
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(255))
     password = db.Column(db.String(255))
+    posts = db.relationship(
+        'Post',
+        backref='user',
+        lazy='dynamic'
+    )
 
     def __init__(self, username):
         self.username = username
--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
-from main import app, db, User
+from main import app, Comment, db, Post, User
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app, db=db, User=User)
+    return dict(app=app, Comment=Comment, db=db, Post=Post, User=User)
